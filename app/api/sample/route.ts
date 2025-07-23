40d202fe9a12597928fd0062045518a89a98d77e
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-07-23 12:05:33 +0530

    file -fix

--- app/api/sample/route.ts
+++ app/api/sample/route.ts
@@ -1,23 +1,25 @@
 import { NextResponse } from "next/server"
 import fs, { writeFileSync } from "fs"
 import { parse } from "csv-parse/sync"
+import { join } from 'path';
 
 export async function GET() {
-	const client_path = "app/api/sample/client.csv"
-	const task_path = "app/api/sample/tasks.csv"
-	const worker_path = "app/api/sample/workers.csv"
+	
+	const client_path = join(process.cwd(), 'public', 'sample', 'client.csv');
+	const task_path = join(process.cwd(), 'public', 'sample', 'tasks.csv');
+	const worker_path = join(process.cwd(), 'public', 'sample', 'workers.csv');
 	const filePath = "app/api/data.json"
 	try {
 		const client_csv = fs.readFileSync(client_path, "utf-8")
 		const task_csv = fs.readFileSync(task_path, "utf-8")
 		const worker_csv = fs.readFileSync(worker_path, "utf-8")
-                const data = {
-                        clients:[],
-                        workers:[],
-                        tasks:[]
+		const data = {
+			clients: [],
+			workers: [],
+			tasks: []
 
 
-                }
+		}
 
 		data.clients = parse(client_csv, {
 			columns: true,
